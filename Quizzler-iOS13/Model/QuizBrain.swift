6b60c1c9b148769035e0c571c104d7ac200b6798
elahe <el.sh192014@gmail.com>
2022-05-18 22:51:45 +0200

    Quizzler MultipleChoice Elahe edition

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 struct QuizBrain {
+    
     let quiz = [
         Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
         Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100"),
